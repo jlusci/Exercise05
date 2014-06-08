f7e1e15a912ee4401dfd8fcec78891f242ba74dc
Jessica Uscinski <jlusci@Jessicas-MacBook-Air.local>
2014-06-07 17:41:57 -0700

    made a list of ascii characters for a-z

---
+++ lettercount.py
@@ -0,0 +1,31 @@
+from sys import argv
+
+script, filename = argv
+
+def main():
+	my_file = open(filename)
+	print "Opening the file %r" % filename
+	indata = my_file.read()
+	lowered = indata.lower()
+	my_list = list(lowered)
+
+	# print ord(my_list[23567])
+	# lowered = indata.lower()
+	# print lowered
+	# print ord('a')
+	ascii_nums = range(97,123)
+
+	file_ascii_values = list()
+	
+	for index in range(len(my_list)):
+		# print ord(my_list[index])
+		if ord(my_list[index]) in ascii_nums:
+			file_ascii_values.append(ord(my_list[index]))
+		index += 1
+
+	print file_ascii_values
+	# print ord(my_list[676417])	
+
+
+if __name__ == '__main__':
+	main()
